ddb50ed3dc602766e111a3c1cb3edbb3f2a835e7
krove <krove@DESKTOP-Q5SED1R>
2020-10-23 11:38:59 +0530

    [Kashif]Added:Take multiple rides and calculate total for all

--- src/main/java/com/cg/InvoiceGenerator.java
+++ src/main/java/com/cg/InvoiceGenerator.java
@@ -1,11 +1,19 @@
 package com.cg;
 
 public class InvoiceGenerator {
-	public final double COST_PER_KILOMETER = 10.0;
+	private static final double COST_PER_KILOMETER = 10.0;
 	private static final int COST_PER_MINUTE = 1;
+	private static final double MINIMUM_RIDE_FARE = 5.0;
 
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_MINUTE;
+		return Math.max(MINIMUM_RIDE_FARE, totalFare);
+	}
+
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0.0;
+		for(Ride ride : rides)
+			totalFare += calculateFare(ride.distance , ride.time);
 		return totalFare;
 	}
 }
